8e84d810761a8c6f3a5a32fdf0f0039d9341428c
BT_3W2_18 <mohammadi55@nycstudents.net>
2022-09-28 11:39:41 -0400

    c

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class HelloWorld {
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello World!");
+        System.out.println(1+2);
+        int number = input.nextInt();
+        System.out.println(4+number);
+    }
 }
